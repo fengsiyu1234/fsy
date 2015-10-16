cf6007590d35766d5261e3b10e64738286259d49
Administrator <Administrator@Fsy>
2015-10-16 14:02:42 +0800

    新建

---
+++ aaa/src/com/pro/domian/Users.java
@@ -0,0 +1,14 @@
+package com.pro.domian;
+
+public class Users {
+	private int userId;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+}
